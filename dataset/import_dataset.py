ea5eac41794716bd9e87818121608e43e0dc5e7b
Adri1mines <adrien.le_marchand@etu.minesparis.psl.eu>
2026-01-22 22:17:18 +0100

    code pour importer la database pour le pretrain

---
+++ dataset/import_dataset.py
@@ -0,0 +1,105 @@
+import requests
+import json
+import os
+from tqdm import tqdm
+import json
+import gzip
+import shutil
+
+JSON_PATH = "dataset/import_parameters.json"
+TARGET = 100
+
+with open(JSON_PATH, 'r') as f:
+    dic_path = json.load(f)
+# URL de l'index officiel des génomes bactériens RefSeq
+SUMMARY_URL = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt"
+FILEPATH_DATA = dic_path["filepath_data"] + f"raw_genome_{TARGET}_species.txt"
+FILEPATH_META = dic_path["filepath_meta"] + f"organism_list.txt"
+
+
+def main():
+    print("Téléchargement de l'index NCBI en cours... (ça peut prendre quelques secondes)")
+
+    # On récupère le fichier
+    response = requests.get(SUMMARY_URL)
+    data = response.text.splitlines()
+
+    urls = []
+    count = 0
+
+    if os.path.exists(FILEPATH_META):
+        os.remove(FILEPATH_META)
+
+    if os.path.exists(FILEPATH_DATA):
+        os.remove(FILEPATH_DATA)
+
+    print("Filtrage et construction des URLs...")
+    with tqdm(total = TARGET, desc="Progression", unit="souche") as pbar:
+        for line in data:
+            if line.startswith("#"):
+                continue
+            
+            parts = line.split("\t")
+            
+            # Colonnes clés dans assembly_summary.txt :
+            # 7: Organism name
+            # 11: Assembly level (on veut "Complete Genome" ou "Chromosome")
+            # 19: FTP path
+            
+            organism = parts[7]
+            assembly_level = parts[11]
+            ftp_path = parts[19]
+            
+            # On cherche Escherichia coli et on privilégie les génomes complets pour éviter les fragments
+            if "Escherichia coli" in organism and assembly_level == "Complete Genome":
+
+                # Le chemin dans le fichier est en ftp://, on le passe en https://
+                https_base = ftp_path.replace("ftp://", "https://")
+                
+                # On récupère le nom du dossier final (ex: GCF_000013425.1_ASM1342v1)
+                folder_name = https_base.split("/")[-1]
+                
+                # On construit le lien complet vers le fichier .fna.gz
+                full_url = f"{https_base}/{folder_name}_genomic.fna.gz"
+                
+                urls.append(full_url)
+                with open(FILEPATH_DATA, 'a') as f:
+                    with requests.get(full_url, stream = True) as r:
+                        r.raise_for_status()
+                        with gzip.open(r.raw, mode='rt', encoding='utf-8') as f_in:
+        
+                            # --- RÉCUPÉRATION DU NOM DE LA SOUCHE ---
+                            # On lit juste la première ligne
+                            first_line = f_in.readline()
+    
+                            if first_line.startswith(">"):
+                                # On nettoie le chevron '>' et on garde le nom
+                                strain_name = first_line.strip().replace(">", "")
+                            else:
+                                strain_name = "Inconnue"
+
+                            with open(FILEPATH_META, 'a') as f_m:
+                                f_m.write(strain_name + '\n')
+                                
+                                # Optionnel : Tu peux écrire le nom de la souche comme séparateur
+                            f.write(f"\n<START_STRAIN:{strain_name}>\n")
+                                
+                                # On lit le reste du flux ligne par ligne ou par blocs
+                                # copyfileobj est super optimisé pour ça (il lit/écrit par chunks tout seul)
+                            shutil.copyfileobj(f_in, f)
+                                            # for chunk in r.iter_content(chunk_size=8192):
+                                            #     decompressed_chunk = decompressor.decompress(chunk)                            
+                                            #     decompressed_chunk = decompressed_chunk.decode("utf-8")
+                                            #     if not decompressed_chunk:
+                                            #         continue
+                                            #     print("ADN: " + decompressed_chunk)
+                                            #     f.write(decompressed_chunk)
+                count += 1
+                pbar.update(1)
+            
+            if count >= TARGET:
+                break
+
+
+if __name__ == "__main__":
+    main()
