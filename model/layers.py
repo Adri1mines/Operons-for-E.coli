04d0a89e1133e7f2ad365832e2dd48437baaed03
Adri1mines <adrien.le_marchand@etu.minesparis.psl.eu>
2026-01-25 12:01:30 +0100

    implémentation maison d'une architecture transformer classique

--- model/layers.py
+++ model/layers.py
@@ -2,10 +2,12 @@ import torch
 import torch.nn as nn
 import lightning as pl
 from math import sqrt
+import torch.nn.Functional as F
 
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, d_model, n_head):
+        super().__init__()
         self.n_head = n_head
         self.d_k = d_model//n_head
         self.q = nn.Linear(d_model, d_model)
@@ -20,25 +22,63 @@ class CausalSelfAttention(nn.Module):
         k = k.view(x.size(0), x.size(1), self.n_head, -1).transpose(1, 2)
         v = v.view(x.size(0), x.size(1), self.n_head, -1).transpose(1, 2)
         qk = q@k.transpose(2, 3)/sqrt(self.d_k)
-        mask = torch.ones((qk.size(2), qk.size(3)))
-        qk = qk.masked_fill(torch.triu(mask) == 1, float('-inf'))
-        qk = qk.masked_fill(pad_mask == 1, float('-inf'))
-        att = torch.softmax(qk)@v
+        mask = torch.ones((qk.size(2), qk.size(3)), device = x.device)
+        mask_pad = pad_mask.unsqueeze(1).unsqueeze(2)
+        qk = qk.masked_fill(torch.triu(mask, diagonal = 1) == 1, float('-inf'))
+        qk = qk.masked_fill(mask_pad == 1, float('-inf'))
+        att = torch.softmax(qk, dim = -1)@v
+        att = att.transpose(1, 2)
+        att = att.reshape(x.size(0), x.size(1), -1)
+        return att
 
 
 class DNATransformer(pl.LightningModule):
-    def __init__(self, vocab_size, d_model, max_len):
+    def __init__(self, vocab_size, d_model,n_head, max_len, tokenizer):
         super().__init__()
         self.vocab_size = vocab_size
         self.d_model = d_model
         self.max_len = max_len
+        self.pad_token = tokenizer.token_to_id("[PAD]")
         self.embedding = nn.Embedding(vocab_size, d_model)
         self.pos_embedding = nn.Embedding(max_len, d_model)
         self.emb_to_head = nn.Linear(d_model, d_model)
+        self.norm = nn.LayerNorm(d_model)
+        self.attention_head = CausalSelfAttention(d_model, n_head)
+        self.feed_forward = nn.Sequential(nn.Linear(d_model, 4*d_model), nn.ReLU(), nn.Linear(4*d_model, d_model))
+        self.proj = nn.Linear(d_model, vocab_size)
 
-    def forward(self, x, mask):
+
+    def forward(self, x):
         token_emb = self.embedding(x)
         pos_tokens = torch.arange(x.size(1), device = x.device)
         pos_emb = self.pos_embedding(pos_tokens)
         emb = token_emb + pos_emb
-        to_attention = self.emb_to_head(emb)
+        norm_emb = self.norm(emb)
+        to_attention = self.emb_to_head(norm_emb)
+        pad_mask = torch.zeros(x.size(), device = x.device).masked_fill(x == self.pad_token, 1)
+        attention = self.attention_head(to_attention, pad_mask)
+        emb = emb + attention
+        feed_forwarded = self.feed_forward(emb)
+        emb = emb + feed_forwarded
+        logits = self.proj(emb)
+        return logits
+    
+    def training_step(self, batch, _):
+
+        x = batch
+        input = x[:, :-1]
+        target = x[:, 1:]
+        logits = self.forward(input)
+        loss = F.cross_entropy(logits.view(-1, self.vocab_size), target.view(-1))
+        self.log("train_loss", loss, prog_bar = True)
+        return loss
+    
+    def configure_optimizers(self):
+        optimiser = torch.optim.AdamW(self.parameters(), lr = 1e-3)
+        return optimiser
+
+
+
+
+
+
